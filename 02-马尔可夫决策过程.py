c9c23326f207590bb93b67ab80aa5dafd4bc749d
MicroYui <1048354041@qq.com>
2024-08-05 12:06:42 +0800

    learn DRL

---
+++ 02-马尔可夫决策过程.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# 定义好马尔科夫链的马尔可夫奖励过程 <S, P, r, gamma>
+# 其中S是状态集合，P是状态转移概率矩阵，r是进入每个状态的奖励函数，gamma是折扣因子
+np.random.seed(0)
+# 定义状态转移概率矩阵P
+P = [
+    [0.9, 0.1, 0.0, 0.0, 0.0, 0.0],
+    [0.5, 0.0, 0.5, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.6, 0.0, 0.4],
+    [0.0, 0.0, 0.0, 0.0, 0.3, 0.7],
+    [0.0, 0.2, 0.3, 0.5, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+]
+P = np.array(P)
+
+rewards = [-1, -2, -2, 10, 1, 0]  # 定义奖励函数
+gamma = 0.5  # 定义折扣因子
+
+
+# 给定一条序列,计算从某个索引（起始状态）开始到序列最后（终止状态）得到的回报
+def compute_return(start_index, chain, gamma):
+    G = 0
+    for i in reversed(range(start_index, len(chain))):
+        G = gamma * G + rewards[chain[i] - 1]
+    return G
